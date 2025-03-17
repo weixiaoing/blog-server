b6868269beadaf31846a6c22affc7c9f52671cc9
dawnot <1537476031@qq.com>
2025-03-17 15:39:22 +0800

    feat: add env

--- app/config/env.ts
+++ app/config/env.ts
@@ -5,9 +5,9 @@ const envSchema = z.object({
   MONGO_URI: z.string(),
   SERVER_PORT: z.string(),
   SOCKET_PORT: z.string(),
-  AUTH_SECRET: z.string(),
-  AUTH_GITHUB_ID: z.string(),
-  AUTH_GITHUB_SECRET: z.string(),
+  // AUTH_SECRET: z.string(),
+  // AUTH_GITHUB_ID: z.string(),
+  // AUTH_GITHUB_SECRET: z.string(),
 });
 
 const env = envSchema.parse(process.env);
